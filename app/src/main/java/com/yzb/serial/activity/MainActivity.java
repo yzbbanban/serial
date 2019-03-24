709c3460b0548651bc0ae98bc4dc43310db41fcd
yzbbanban <yzbbanban@live.com>
2019-03-24 23:23:28 +0800

    adasdadw

--- app/src/main/java/com/yzb/serial/activity/MainActivity.java
+++ app/src/main/java/com/yzb/serial/activity/MainActivity.java
@@ -367,9 +367,9 @@ public class MainActivity extends BaseActivity implements ICallBack {
             public void onClick(View view) {
 //                ToastUtil.showShortToast("Add");
                 String name = etDialogName.getText().toString().trim();
-                if ("".equals(name) || name.split(",").length < 5) {
-                    ToastUtil.showShortToast("请输入正确格式：如 P0TTT,30111111,2017.06.29,20,2017.06.29");
-                } else {
+//                if ("".equals(name) || name.split(",").length < 5) {
+//                    ToastUtil.showShortToast("请输入正确格式：如 P0TTT,30111111,2017.06.29,20,2017.06.29");
+//                } else {
                     //比对
                     bucket.setName(name);
 
@@ -382,7 +382,7 @@ public class MainActivity extends BaseActivity implements ICallBack {
                     }
                     ToastUtil.showLongToastTop("比对中");
                     sendOperaModel.compare(bucket, MainActivity.this);
-                }
+//                }
 
             }
         });
